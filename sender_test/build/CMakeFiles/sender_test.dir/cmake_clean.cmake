62d56f82066c369a1430721042ca548c0b5c65fb
adepe <augustin.deper@hotmail.fr>
2024-03-02 16:54:51 +0100

    print

---
+++ sender_test/build/CMakeFiles/sender_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sender_test.dir/main.cpp.o"
+  "CMakeFiles/sender_test.dir/main.cpp.o.d"
+  "sender_test"
+  "sender_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sender_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sender_test/main.cpp
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <vector>
+#include <cstdlib>
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) { // Vérification du nombre d'arguments
+        fprintf(stderr, "Usage: %s <write pipe fd>\n", argv[0]); // Affichage d'un message d'erreur si le nombre d'arguments est incorrect
+        exit(1); // Arrêt du programme
+    }
+
+    int write_fd = atoi(argv[1]); // Récupération du descripteur de fichier d'écriture du pipe à partir des arguments de la ligne de commande
+
+    std::vector<float> numbers = {1.1, 2.2, 3.3, 4.4, 5.5}; // Déclaration du tableau de nombres à virgule flottante à envoyer
+    write(write_fd, numbers.data(), numbers.size() * sizeof(float)); // Écriture des nombres dans le pipe
+    sleep(3);
+    numbers = {6,5,4,32}; // Déclaration du tableau de nombres à virgule flottante à envoyer
+    write(write_fd, numbers.data(), numbers.size() * sizeof(float)); // Écriture des nombres dans le pipe
+
+    close(write_fd); // Fermeture du descripteur de fichier d'écriture du pipe
+
+    return 0;
+}
