9b370d5ec5327eb5272e149c601ef08a4c4a439c
adepe <augustin.deper@hotmail.fr>
2024-03-03 16:57:28 +0100

    POTENTIAL FIELD MA GUEULE

--- main_controller/fieldpath.c
+++ main_controller/fieldpath.c
@@ -56,13 +56,13 @@ void updateRepulsiveField(int x1,int y1, int x2, int y2){
         for (int j = activeX1; j < activeX2; ++j) {
             euclidianDistance = computeRectangleDistance(X1,Y1,X2,Y2,j,i);
             if(j >= X1 && j <= X2 && i >= Y1 && i <= Y2){
-                myField.repulsiveField[i][j] = 1/2 * pow((1-(1/actionDistance)),2);
+                myField.repulsiveField[i][j] = 0.5*pow((1-(1/actionDistance)),2);
                 fprintf(stderr,"repulsive in x = %d and y = %d is updated to %lf \n",j,i,myField.repulsiveField[i][j]);
                 fprintf(stderr,"Computation was %lf \n",0.5*pow((1-(1/actionDistance)),2));
             } //Si on est DANS la zone de l'objet
 
             else if(euclidianDistance <= actionDistance){
-                myField.repulsiveField[i][j] = 1/2 *pow((1/euclidianDistance - 1/actionDistance),2);
+                myField.repulsiveField[i][j] = 0.5*pow(((1/euclidianDistance)-(1/actionDistance)),2);
                 fprintf(stderr,"repulsive in x = %d and y = %d is updated to %lf \n",j,i,myField.repulsiveField[i][j]);
             }
             else myField.repulsiveField[i][j] = 0;
