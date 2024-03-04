5d906873eb16e531b187e57b0e0f79486be46f59
adepe <augustin.deper@hotmail.fr>
2024-03-04 12:00:48 +0100

    POTENTIAL FIELD MA GUEULE

--- main_controller/fieldpath.c
+++ main_controller/fieldpath.c
@@ -168,7 +168,7 @@ void addObstacle(double posX, double posY, double size, uint8_t moving){
         fprintf(stderr,"Obstacle added, size of list was 0 and is 1 now\n");
     }
     else{
-
+        fprintf("Realloc problem 1 \n");
         myForce.obstacleList = realloc(myForce.obstacleList,sizeof(obstacle)*(myForce.obstacleNumber)+1);
         myForce.obstacleList[myForce.obstacleNumber] = myObstacle;
         myForce.obstacleNumber ++;
@@ -179,6 +179,7 @@ void addObstacle(double posX, double posY, double size, uint8_t moving){
             myForce.movingNumber ++;
         }
         else{
+            fprintf("Realloc problem 2 \n");
             myForce.movingIndexes = (int*) realloc(myForce.movingIndexes,sizeof(int)*(myForce.movingNumber+1));
             myForce.movingIndexes[myForce.obstacleNumber] = myForce.obstacleNumber-1;
             myForce.movingNumber ++;
@@ -197,6 +198,7 @@ void removeMovingObstacles(){
         myForce.obstacleNumber --;
         myForce.movingNumber --;
     }
+    fprintf("Realloc problem 3 \n");
     myForce.obstacleList = realloc(myForce.obstacleList,sizeof(obstacle)*myForce.obstacleNumber);
 
 }
