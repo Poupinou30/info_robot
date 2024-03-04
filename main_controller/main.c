08a09294599e3e652607e46946ee66fef31cb4d9
adepe <augustin.deper@hotmail.fr>
2024-03-04 13:57:44 +0100

    POTENTIAL FIELD MA GUEULE

--- main_controller/main.c
+++ main_controller/main.c
@@ -23,6 +23,7 @@ int main(){
     *myPos.y = 0;
     *myPos.theta = 0;
     //addObstacle(0.12,0.42,1,0);
+    computeForceVector();
     fprintf(stderr,"Initial force X  = %lf \n",f_tot_x);
     fprintf(stderr,"Initial force Y  = %lf \n",f_tot_y);
 
