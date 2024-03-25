dac38ac1fe33bf0b83e1274eb6ea2152711790d1
Poupinou30 <alicia.torck@student.uclouvain.be>
2024-03-25 11:27:27 +0100

    Nouveaux codes alignés sur le tag

--- Cam_dir/agUrco.py
+++ Cam_dir/agUrco.py
@@ -22,6 +22,7 @@ while True:
 	#print(corners)
 	#ici c'est la commande pour calibrer et pouvoir compenser la distorsion etc
 	#ret, mtx, dist, rvecs, tvecs = cv/calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+	trav=[]
 	if ids is not None :
 		print("il y a " + str(len(ids)) + " plantes")
 		cv2.aruco.drawDetectedMarkers(frame, corners, ids)
@@ -30,7 +31,7 @@ while True:
 		rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, markerSizeInM,mtx,dist)
 		#print(rvec)
 		#print(tvec)
-		trav=[]
+		
 		for i in range(len(ids)):
 			angle=rvec[i][0][0]*(180.0/np.pi)
 			if(angle<=140 and angle>=60 or angle<=-60 and angle>=-140):
@@ -50,7 +51,30 @@ while True:
 		for k in range(len(trav)):
 			coord_trav.append(coordinate[k])
 			tag_corners=(corners[trav[k]][0])
-			cv2.polylines(mask, [np.int32(tag_corners)], isClosed=True, color=(0,0,255), thickness=4)
+			"""
+			on calcule le centre du tag
+			"""
+			tag_center = np.mean(tag_corners, axis=0)
+			rect_height=10
+			rect_width= 10
+			"""
+			on convertit en taille de pixels
+			"""
+			print("haut gauche : " +str(tag_corners[0]))
+			print("haut droite : " +str(tag_corners[1]))
+			print("bas gauche : " +str(tag_corners[3]))
+			print("bas droite : " +str(tag_corners[2]))
+			
+			cm_per_pixel= markerSizeInM*200/30
+			
+			h_px= int(rect_height/cm_per_pixel)
+			
+			w_px= int(rect_width/cm_per_pixel)
+			top_left_corner = (int(tag_center[0] - w_px / 2), int(tag_center[1] - h_px / 2))
+			bottom_right_corner = (int(tag_center[0] + w_px / 2), int(tag_center[1] + h_px / 2))
+			
+			#cv2.polylines(mask, [np.int32(tag_corners)], isClosed=True, color=(0,0,255), thickness=4)
+			cv2.rectangle(mask, top_left_corner, bottom_right_corner, color=(0,0,255), thickness=4)
 			output = cv2.add(frame, mask)
 			#print(tag_corners)
 		#print("les coords des pots de travers: " + str(coord_trav) + " \n")
