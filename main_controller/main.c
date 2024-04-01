729b447766f890144b652709e7a80424af8cbd0a
adepe <augustin.deper@hotmail.fr>
2024-04-01 20:32:47 +0200

    Increase sleep time in main loop

--- main_controller/main.c
+++ main_controller/main.c
@@ -94,7 +94,7 @@ int main(){
         printf("x = %f y = %f theta = %f \n",*myFilteredPos.x,*myFilteredPos.y,*myFilteredPos.theta);
         printf("x odo = %f y odo = %f theta odo = %f \n",*myOdometryPos.x,*myOdometryPos.y,*myOdometryPos.theta);
         printf("lidar x = %f y = %f theta = %f \n",*myPos.x,*myPos.y,*myPos.theta);
-        sleep(0.5);
+        sleep(1);
     }
     
 }
