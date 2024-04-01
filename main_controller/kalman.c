8e4d69c63c103ee98da27b836cbe90fcfe9aabd8
adepe <augustin.deper@hotmail.fr>
2024-04-01 20:27:36 +0200

    t

--- main_controller/kalman.c
+++ main_controller/kalman.c
@@ -42,27 +42,32 @@ void* updateKalman(void* args) {
     }
 
     // Étape de mise à jour
+    double K[3][6];
     for (int i = 0; i < 6; i++) {
         double y = measurementsCombined[i] - (H[i][0] * x_pred[0] + H[i][1] * x_pred[1] + H[i][2] * x_pred[2]); // Innovation
         double S = H[i][0] * P_pred[0][0] * H[i][0] + H[i][1] * P_pred[1][1] * H[i][1] + H[i][2] * P_pred[2][2] * H[i][2] + R[i]; // Covariance de l'innovation
-        double K[3];
         for (int j = 0; j < 3; j++) {
-            K[j] = P_pred[j][0] * H[i][0] / S; // Gain de Kalman
-            x[j] = x_pred[j] + K[j] * y; // Mise à jour de l'état
-            P[j][0] = P_pred[j][0] - K[j] * H[i][0] * P_pred[j][0]; // Mise à jour de la covariance de l'état
-            // Mise à jour des autres éléments de la matrice de covariance de l'état
-            for (int k = 1; k < 3; k++) {
-                P[j][k] = P_pred[j][k] - K[j] * H[i][k] * P_pred[j][k];
+            K[j][i] = P_pred[j][0] * H[i][0] / S; // Gain de Kalman
+        }
+    }
+    for (int j = 0; j < 3; j++) {
+        x[j] = x_pred[j];
+        for (int i = 0; i < 6; i++) {
+            x[j] += K[j][i] * (measurementsCombined[i] - (H[i][0] * x_pred[0] + H[i][1] * x_pred[1] + H[i][2] * x_pred[2])); // Mise à jour de l'état
+        }
+        for (int k = 0; k < 3; k++) {
+            P[j][k] = P_pred[j][k];
+            for (int i = 0; i < 6; i++) {
+                P[j][k] -= K[j][i] * H[i][k] * P_pred[j][k]; // Mise à jour de la covariance de l'état
             }
         }
     }
 
     pthread_mutex_lock(&lockFilteredPosition);
-    //fprintf(stderr, "x filtered test = %lf \n", x[0]);
     *(myFilteredPos.x) = x[0];
     *(myFilteredPos.y) = x[1];
     *(myFilteredPos.theta) = x[2];
     pthread_mutex_unlock(&lockFilteredPosition);
 
-    return NULL; // Assurez-vous que la fonction correspond à la signature attendue
+    return NULL;
 }
