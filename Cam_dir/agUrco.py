b33797cee470c77c5d735ae9262e4938bea5f99b
Poupinou30 <alicia.torck@student.uclouvain.be>
2024-04-15 15:36:07 +0200

    Transférer une liste de coordonnées du code python vers le code c"

--- Cam_dir/agUrco.py
+++ Cam_dir/agUrco.py
@@ -3,125 +3,154 @@
 import cv2
 import numpy as np
 
-cap= cv2.VideoCapture(0)
+def data_cam():
+	
+	cap= cv2.VideoCapture(0)
+	aruco_dict= cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+	parameters = cv2.aruco.DetectorParameters_create()
+	fx=500
+	fy=500
+	cx=320
+	cy=240
+	mtx = np.array([[fx, 0, cx], [0, fx, cy], [0,0,1]])
+	dist = np.zeros((4,1))
 
-aruco_dict= cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
-parameters = cv2.aruco.DetectorParameters_create()
-fx=500
-fy=500
-cx=320
-cy=240
-mtx = np.array([[fx, 0, cx], [0, fx, cy], [0,0,1]])
-dist = np.zeros((4,1))
-
-while True:
-	ret, frame= cap.read()
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-	mask = np.zeros_like(frame)
-	#print(corners)
-	#ici c'est la commande pour calibrer et pouvoir compenser la distorsion etc
-	#ret, mtx, dist, rvecs, tvecs = cv/calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-	trav=[]
-	if ids is not None :
-		print("il y a " + str(len(ids)) + " plantes")
-		cv2.aruco.drawDetectedMarkers(frame, corners, ids)
-		coordinate=[]#ICI POUR LES COORDS DE TOUTES LES PLANTES
-		markerSizeInM=0.02
-		rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, markerSizeInM,mtx,dist)
-		#print(rvec)
-		#print(tvec)
-		
-		for i in range(len(ids)):
-			if(ids[i][0]==13 or ids[i][0]==36):
-				angle=rvec[i][0][0]*(180.0/np.pi)
-				if(angle<=140 and angle>=60 or angle<=-60 and angle>=-140):
-					#print("la plante est de traviole attention")
-					trav.append(i)
-				coord=[]
-				coord.append(tvec[i][0].tolist())
-				coord.append(rvec[i][0].tolist())
-			coordinate.append(coord)
-			#print(str(coord)+" \n")
-		#print(rvec)
-		#print(tvec)
-		#print(coordinate)
-		#print(trav)
-		#print("coordinates of the " + str(len(ids)) + " plants :"   + str(coordinate)+ " \n")
-		coord_trav=[] #ICI POUR LES COORDONNEES DES PLANTES QUI SONT DE TRAVERS
-		for k in range(len(trav)):
-			coord_trav.append(coordinate[k])
-			tag_corners=(corners[trav[k]][0])
-			"""
-			on calcule le centre du tag
-			"""
-			tag_center = np.mean(tag_corners, axis=0)
-			rect_height=10
-			rect_width= 10
-			"""
-			on convertit en taille de pixels
-			"""
-			print("haut gauche : " +str(tag_corners[0]))
-			print("haut droite : " +str(tag_corners[1]))
-			print("bas gauche : " +str(tag_corners[3]))
-			print("bas droite : " +str(tag_corners[2]))
-			
-			cm_per_pixel= markerSizeInM*200/30
-			
-			h_px= int(rect_height/cm_per_pixel)
-			
-			w_px= int(rect_width/cm_per_pixel)
-			top_left_corner = (int(tag_center[0] - w_px / 2), int(tag_center[1] - h_px / 2))
-			bottom_right_corner = (int(tag_center[0] + w_px / 2), int(tag_center[1] + h_px / 2))
-			
-			#cv2.polylines(mask, [np.int32(tag_corners)], isClosed=True, color=(0,0,255), thickness=4)
-			cv2.rectangle(mask, top_left_corner, bottom_right_corner, color=(0,0,255), thickness=4)
-			output = cv2.add(frame, mask)
-			#print(tag_corners)
-		#print("les coords des pots de travers: " + str(coord_trav) + " \n")
-		print(" les tailles des listes: toutes les plantes : " + str(len(coordinate)) + " et juste les plantes de travers : " + str(len(coord_trav) )+ " \n")
-		"""
-		for i in range(len(ids)):
-			r_deg=[]
-			for j in range(3):
-				r_deg.append(rvec[0][i][j]*(180.0/np.pi))
-		#print(r_deg)
-		if(r_deg[0]<=185 and r_deg[0]>=175 or r_deg[0]<=-175 and r_deg[0]>=-185):
-			print("la plante est droite")
-		if(r_deg[0]<=120 and r_deg[0]>=80 or r_deg[0]<=-80 and r_deg[0]>=-120):
-			print("la plante est de traviole attention")
-		
-		for i in range(len(ids)) :
+	while True:
+		ret, frame= cap.read()
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+		mask = np.zeros_like(frame)
+		#print(corners)
+		#ici c'est la commande pour calibrer et pouvoir compenser la distorsion etc
+		#ret, mtx, dist, rvecs, tvecs = cv/calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+		trav=[]
+		if ids is not None :
+			#print("il y a " + str(len(ids)) + " plantes")
+			cv2.aruco.drawDetectedMarkers(frame, corners, ids)
+			coordinate=[]#ICI POUR LES COORDS DE TOUTES LES PLANTES
 			markerSizeInM=0.02
 			rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, markerSizeInM,mtx,dist)
-			print(tvec)
-			coordinate.append(tvec[0][0])
-		print(coordinate)
-		"""
-		"""
-			if(id_tag[0]==13):
-				#print("plante mauve:")
-				markerSizeInM=0.02
+			#print(rvec)
+			#print(tvec)
+			
+			for i in range(len(ids)):
+				if(ids[i][0]==13 or ids[i][0]==36):
+					angle=rvec[i][0][0]*(180.0/np.pi)
+					if(angle<=140 and angle>=60 or angle<=-60 and angle>=-140):
+						#print("la plante est de traviole attention")
+						trav.append(i)
+					coord=[]
+					coord.append(tvec[i][0].tolist())
+					#print(coord[0])
+					#coord.append(rvec[i][0].tolist())
+				#print(coord)
+				coordinate.append(coord[0])
+				#print(str(coord)+" \n")
+			if(len(ids)!=0):
+				return coordinate
+			#print(rvec)
+			#print(tvec)
+			#print(coordinate)
+			#print(trav)
+			#print("coordinates of the " + str(len(ids)) + " plants :"   + str(coordinate)+ " \n")
+			coord_trav=[] #ICI POUR LES COORDONNEES DES PLANTES QUI SONT DE TRAVERS
+			for k in range(len(trav)):
+				coord_trav.append(coordinate[k])
+				tag_corners=(corners[trav[k]][0])
+				"""
+				on calcule le centre du tag
+				"""
+				tag_center = np.mean(tag_corners, axis=0)
+				rect_height=10
+				rect_width= 10
+				"""
+				on convertit en taille de pixels
+				"""
+				"""
+				print("haut gauche : " +str(tag_corners[0]))
+				print("haut droite : " +str(tag_corners[1]))
+				print("bas gauche : " +str(tag_corners[3]))
+				print("bas droite : " +str(tag_corners[2]))
+				"""
+				cm_per_pixel= markerSizeInM*200/30
 				
-				rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, markerSizeInM,mtx,dist)
-				#print("Violet Plant coordinates (x,y,z):"+ str(tvec[0][0]) + " and rotation: " + str(rvec) +"\n") #
+				h_px= int(rect_height/cm_per_pixel)
 				
-			if(id_tag[0]==36):
+				w_px= int(rect_width/cm_per_pixel)
+				top_left_corner = (int(tag_center[0] - w_px / 2), int(tag_center[1] - h_px / 2))
+				bottom_right_corner = (int(tag_center[0] + w_px / 2), int(tag_center[1] + h_px / 2))
+				
+				#cv2.polylines(mask, [np.int32(tag_corners)], isClosed=True, color=(0,0,255), thickness=4)
+				cv2.rectangle(mask, top_left_corner, bottom_right_corner, color=(0,0,255), thickness=4)
+				output = cv2.add(frame, mask)
+				#print(tag_corners)
+			#print("les coords des pots de travers: " + str(coord_trav) + " \n")
+			#print(" les tailles des listes: toutes les plantes : " + str(len(coordinate)) + " et juste les plantes de travers : " + str(len(coord_trav) )+ " \n")
+			"""
+			for i in range(len(ids)):
+				r_deg=[]
+				for j in range(3):
+					r_deg.append(rvec[0][i][j]*(180.0/np.pi))
+			#print(r_deg)
+			if(r_deg[0]<=185 and r_deg[0]>=175 or r_deg[0]<=-175 and r_deg[0]>=-185):
+				print("la plante est droite")
+			if(r_deg[0]<=120 and r_deg[0]>=80 or r_deg[0]<=-80 and r_deg[0]>=-120):
+				print("la plante est de traviole attention")
+			
+			for i in range(len(ids)) :
 				markerSizeInM=0.02
 				rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, markerSizeInM,mtx,dist)
-				#print("White Plant coordinates (x,y,z):"+ str(tvec[0][0]) + " and rotation: " + str(rvec) +"\n") #
-			if(id_tag[0]==47):
-				markerSizeInM=0.02
-				rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, markerSizeInM,mtx,dist)
-				#print("Solar Panel coordinates (x,y,z):"+ str(tvec[0][0]) + " and rotation: " + str(rvec) +"\n") #
-		"""
-	if(len(trav)!=0):
-		cv2.imshow('Aruco Detection', output)
-	else: 
-		cv2.imshow('Aruco Detection', frame)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+				#print(tvec)
+				#coordinate.append(tvec[0][0])
+				print(coordinate)
+			"""
+			
+			#print(coordinate)
+			"""
+			"""
+			"""
+				if(id_tag[0]==13):
+					#print("plante mauve:")
+					markerSizeInM=0.02
+					
+					rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, markerSizeInM,mtx,dist)
+					#print("Violet Plant coordinates (x,y,z):"+ str(tvec[0][0]) + " and rotation: " + str(rvec) +"\n") #
+					
+				if(id_tag[0]==36):
+					markerSizeInM=0.02
+					rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, markerSizeInM,mtx,dist)
+					#print("White Plant coordinates (x,y,z):"+ str(tvec[0][0]) + " and rotation: " + str(rvec) +"\n") #
+				if(id_tag[0]==47):
+					markerSizeInM=0.02
+					rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, markerSizeInM,mtx,dist)
+					#print("Solar Panel coordinates (x,y,z):"+ str(tvec[0][0]) + " and rotation: " + str(rvec) +"\n") #
+			"""
+		
+			
+		if(len(trav)!=0):
+			cv2.imshow('Aruco Detection', output)
+		else: 
+			cv2.imshow('Aruco Detection', frame)
+		
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			
+			break
 
-cap.release()
-cv2.destroyAllWindows()
+	cap.release()
+	cv2.destroyAllWindows()
+	
 
+if __name__ == "__main__":
+	coordinates= data_cam()
+	print(coordinates)
+	
+	#for coord in coordinates:
+	#	coord_string = " ".join(map(str, coord))
+	#	print(coord_string)
+	
+	#for i in range (len(coordinates)):
+		
+	#print(str(coordinates)) #[0][0][2]
+	#print(coordinates)
+	#coordinates = 1
+	#print("ok")
--- Cam_dir/cam.c
+++ Cam_dir/cam.c
@@ -1,6 +1,13 @@
 //ici c'est cam.c
-#include "cam.cpp"
+//#include "cam.cpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#define MAX_LINE_LENGTH 256
+
 int main(){
+    /*
     int code= system(cam.cpp);
 
     // checking if the command was executed successfully
@@ -12,6 +19,99 @@ int main(){
                 "non-zero: "
              << returnCode << endl;
     }
- 
+    * */
+    FILE *fp;
+    char path[MAX_LINE_LENGTH];
+    //char *token;
+    //int x, y, z;
+    float coordinates[6][3];
+    int num_sublists = 0;
+    fp = popen("python3 agUrco.py", "r");//on ouvre le fichier python
+    if(fp == NULL){
+        printf("Attention le code python run pas \n");
+        return 1;
+    }
+    
+    
+    while(fgets(path, sizeof(path), fp) != NULL){//pourquoi il enlève le -1 maintenant?
+        if(path[0] =='['){
+            int count=0;
+            char *token = strtok(path, "[], \n");
+            while(token!=NULL || count< 3){
+                errno = 0;
+                coordinates[num_sublists][count] = strtof(token, NULL);
+                if(coordinates[num_sublists][count] == 0.0 && errno != 0){
+                    perror("strtof");
+                }
+                token = strtok(NULL, "[], \n");
+                count++;
+            }
+            num_sublists++;
+            if(num_sublists >= 6){
+                printf("chelou ça");
+                break;
+            } 
+        }
+        
+   }
+   printf("%d", num_sublists);
+   for(int i = 0; i < num_sublists;i++){
+        printf("Sublist %d : (%.6f, %.6f, %.6f)\n", i+1, coordinates[i][0], coordinates[i][1], coordinates[i][2]);
+   }     
+        /*
+        if(sscanf(path, "%f %f %f", &x, &y, &z)==3){
+            printf("Received coord: (%.6f, %.6f, %.6f)\n", x, y, z);
+        }else {
+            printf("Error: Failed to parse coord from input.\n");
+        }
+        
+        token = strtok(path, "\n");
+        printf("token: %s\n", token);
+        float coord_list[3]={};
+        int counter=0;
+        //printf("token: %s\n, %d", token);
+        
+        while (token!=NULL && counter <3){
+            printf("ici counter %d", counter);
+            errno = 0;
+            if(counter ==0){
+                x=strtof(token, NULL);
+                printf("%f", x);
+            }
+            else if(counter ==1){
+                y=strtof(token, NULL);
+            }
+            else if(counter ==2){
+                z=strtof(token, NULL);
+            }
+            if(errno !=0){
+                perror("strtof");
+            }
+            
+            token = strtok(NULL, " ");
+            
+            //float float_val= strtof(token, NULL);
+            //printf("Received float value : %.6f\n", float_val);
+            //coord_list[counter]=float_val;
+            counter+=1;
+            * 
+        */   
+        
+        //printf("Received coord: (%.6f, %.6f, %.6f)\n", x, y, z);
+        //printf("%d \n", counter);
+        //printf("la liste: %f, %f, %f \n", coord_list[0], coord_list[1], coord_list[2]);
+        
+        /*
+        //int x, y, z;
+        if(sscanf(path, "%f %f %f", &x, &y, &z)==3){
+            printf("Received coordinates : (%f, %f, %f)\n", x, y, z);
+            //coord_list[0]=x;
+            //coord_list[1]=y;
+            //coord_list[2]=z;
+        */
+        
+    
+    
+    pclose(fp);
     return 0;
 }
