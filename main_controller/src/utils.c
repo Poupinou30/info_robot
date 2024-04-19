a877d0a81b3c2a08ce071623d3441ddbfd674e3f
adepe <augustin.deper@hotmail.fr>
2024-04-19 17:09:30 +0200

    Update omega calculation in computeSpeedFromOdometry in utils.c

--- main_controller/src/utils.c
+++ main_controller/src/utils.c
@@ -95,16 +95,26 @@ void convertsVelocity(double v_x, double v_y, double omega, double* output_speed
 
 }
 
+float oldWheelSpeeds[4] = {0,0,0,0};
+
 void computeSpeedFromOdometry(double* wheel_speeds, double *v_x, double *v_y, double *omega) {
     *v_x = 1.02*1.04*radius/1.0744 / 4 * (wheel_speeds[0] - wheel_speeds[1] - wheel_speeds[2] + wheel_speeds[3]);
 
     *v_y = 1.0167*radius / 4 * (wheel_speeds[0] + wheel_speeds[1] + wheel_speeds[2] + wheel_speeds[3]);
 
+    oldWheelSpeeds[0] = wheel_speeds[0];
+    oldWheelSpeeds[1] = wheel_speeds[1];
+    oldWheelSpeeds[2] = wheel_speeds[2];
+    oldWheelSpeeds[3] = wheel_speeds[3];
+
     for(int i = 0; i<4; i++){
         if(wheel_speeds[i] > 30){
             printf("myWheelSpeed[%d] = %f\n",i,wheel_speeds[i]);
+            wheel_speeds[i] = oldWheelSpeeds[i];
         }
     }
+
+
 
     double tempoOmega =  112.5*/*1.043**/radius / (4 * (l_x + l_y)) * (-wheel_speeds[0] + wheel_speeds[1] - wheel_speeds[2] + wheel_speeds[3]);
 
